90fb32346437be8cd9c845446b5d490c9ea20fbc
psteeep <pstep4e23@gmail.com>
2023-02-22 16:58:09 +0200

    added db

--- Dockerfile
+++ Dockerfile
@@ -9,4 +9,4 @@ COPY . /app
 EXPOSE 8000
 
 
-CMD python3 -m pytest; uvicorn app.main:app --host 0.0.0.0 --port 8000
+CMD python3 -m pytest; uvicorn app.main:app --host 0.0.0.0 --port 8000; python system_config.py
--- app/main.py
+++ app/main.py
@@ -1,6 +1,12 @@
-#import uvicorn
+import uvicorn
 from fastapi import FastAPI
-#import system_config
+import databases
+from app import system_config
+import asyncio
+import aioredis
+
+database = databases.Database(
+    f"postgresql://{system_config.db_user}:{system_config.db_password}@postgres/{system_config.db_db}")
 
 app = FastAPI()
 
@@ -13,3 +19,17 @@ async def health_check():
         "result": "working"
     }
 
+
+@app.on_event("startup")
+async def startup():
+    redis = await aioredis.from_url("redis://redis")
+    await database.connect()
+
+
+@app.on_event("shutdown")
+async def shutdown():
+    await database.disconnect()
+
+
+if __name__ == '__main__':
+    uvicorn.run('main:app', host=system_config.host, port=system_config.dev_port, reload=True)
