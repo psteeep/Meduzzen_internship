c24dcc0c2e48146f3f03e641fb075abdccba9421
psteeep <pstep4e23@gmail.com>
2023-03-22 18:37:15 +0200

    added status code

--- app/routers/users.py
+++ app/routers/users.py
@@ -11,33 +11,33 @@ router = APIRouter()
 token_auth_schema = HTTPBearer()
 
 
-@router.get('/', response_model=List[UserSchema])
+@router.get('/', response_model=List[UserSchema], status_code=200)
 async def get_users(users: UserCRUD = Depends(get_user_crud), limit: int = 100, skip: int = 0):
     return await users.get_users(limit=limit, skip=skip)
 
 
-@router.post('/', response_model=UserSchema)
+@router.post('/', response_model=UserSchema, status_code=201)
 async def create(user: SignUp, users: UserCRUD = Depends(get_user_crud)):
     return await users.create_user(u=user)
 
 
-@router.put('/', response_model=UserSchema)
+@router.put('/', response_model=UserSchema, status_code=200)
 async def update(id: int, user: SignUp, users: UserCRUD = Depends(get_user_crud)):
     return await users.update_user(id=id, u=user)
 
 
-@router.get('/{id}', response_model=UserSchema)
+@router.get('/{id}', response_model=UserSchema, status_code=200)
 async def get_user(id: int, users: UserCRUD = Depends(get_user_crud)) -> UserSchema:
     return await users.get_user(id=id)
 
 
-@router.delete('/')
+@router.delete('/', status_code=204)
 async def delete(id: int, users: UserCRUD = Depends(get_user_crud)) -> UserSchema:
     return await users.delete_user(id=id)
     # return {"message": "Item deleted successfully"}
 
 
-@router.post('/login', response_model=Token)
+@router.post('/login', response_model=Token, status_code=201)
 async def login(login: SignIn, users: UserCRUD = Depends(get_user_crud)):
     user = await users.get_by_email(login.email)
     if user is None:
@@ -48,6 +48,6 @@ async def login(login: SignIn, users: UserCRUD = Depends(get_user_crud)):
     )
 
 
-@router.get('/me/', response_model=UserSchema)
+@router.get('/me/', response_model=UserSchema, status_code=201)
 async def get_mes(users=Depends(get_current_user)):
     return users
